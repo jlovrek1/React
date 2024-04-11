1ecd03cf7c76ac8bfb27f91c02e267b8c6d1c225
jlovrek1 <josephlovrekovic@gmail.com>
2024-04-11 13:30:59 -0400

    react practice

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -6,4 +6,16 @@ function Header(){
         <h1>My Website</h1>
         <nav>
             <ul>
-                <li>
+                <li> <a href="#"> </a>Home</li>
+                <li> <a href="#"> </a>About</li>
+                <li> <a href="#"> </a>Services</li>
+                <li> <a href="#"> </a>Contact</li>
+            </ul>
+        </nav>
+        <hr></hr>
+        </header>
+    );
+
+}
+
+export default Header 
