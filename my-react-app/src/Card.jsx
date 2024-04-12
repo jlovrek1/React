70ef7015b8d00c496d3323068dca1c2a5e249ddf
jlovrek1 <josephlovrekovic@gmail.com>
2024-04-11 23:10:34 -0400

    card

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="profile picture"/>
+            <h2>Joey Lovrekovic</h2>
+            <p>I am a software dev</p>
+        </div>
+    );
+}
+export default Card;
